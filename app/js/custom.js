822b98c53d5954e6a4d9e12ee085eee10ca07db8
Elsevar <rizaeve@bk.ru>
2017-06-18 20:20:51 +0400

    carousel + fix

---
+++ app/js/custom.js
@@ -0,0 +1,14 @@
+/**
+ * Created by elsevar on 18.06.17.
+ */
+$(document).ready(function() {
+    $('#features-auto').owlCarousel({
+        items: 3,
+        loop: true,
+        autoHeight: true
+    });
+    $('#list-auto').owlCarousel({
+        items: 4,
+        loop: true
+    });
+});
